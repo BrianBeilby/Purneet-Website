d2f10213b929d47755f16a7d7b2548585da9e98d
BrianBeilby <brianbeilby98@gmail.com>
2024-06-02 11:18:13 +0900

    Enlarged the Spotify embed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -81,8 +81,8 @@ function Front_Home() {
         <iframe
           className="rounded-xl mt-20"
           src="https://open.spotify.com/embed/episode/1HA82v3w8mWI79Imajzrdm/video?utm_source=generator"
-          width="496"
-          height="279"
+          width="700"
+          height="330"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         ></iframe>
