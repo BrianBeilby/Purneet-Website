bb2d9a57eeacbb320c63f69423ca73254a536100
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-07-13 00:46:45 +0900

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,43 +18,24 @@ function About() {
         Mission Statement
       </h2>
       <p className="mb-6 px-2 sm:px-0">
-        Welcome to the{" "}
-        <span className="font-bold text-orange-700">Dim Eye Show</span>
-        . This platform is an open space for conversation and engagement 
-        about topics that are often overlooked{" "}
-        <span className="font-bold text-orange-700">
-        as well as a platform to explore interesting topics and subjects.
-        </span>{" "}
-        Common themes in our content are: society and societal impacts, technology 
-        & innovation, spirituality/religion/philosophy, and business.{" "}
-        <span className="font-bold text-orange-700">
-        Engagement with experts in their fields, 
-        </span>
-        including business owners and educators help us have thoughtful and 
-        thought-provoking conversations with interesting questions and even 
-        better answers. 
+      Welcome to the Dim Eye Show. This platform is an open space for conversation and engagement about 
+      topics that are often overlooked as well as a platform to explore interesting topics and subjects. 
+      Common themes in our content are: society and societal impacts, technology & innovation, spirituality/religion/philosophy, 
+      and business. Engagement with experts in their fields, including business owners and educators help us have thoughtful and 
+      thought-provoking conversations with interesting questions and even better answers. This podcast style of the show goes deep, 
+      but we also aim to go wide. Content such as street interviews, quick question videos, or random thought videos help us in gaining 
+      and sharing more insight on the human experience and the variability that results from it. 
       </p>
 
-      <p className="mb-3 mt-6 px-2 sm:px-0">
-      <span className="mb-6 px-2 text-orange-700 sfont-bold sm:px-0">
-        This podcast style of the show goes deep,</span>
-        but we also aim to go wide. 
-        Content such as street interviews, quick question videos, or random thought 
-        videos help us in gaining and sharing more {" "}
-        <span className="font-bold text-orange-700">insight on the human experience and 
-        the variability that results from it.</span>
-        Many things in today&apos;s world contribute to making us feel isolated in our struggles and problems, 
-        but that doesn&apos;t mean it&apos;s true. Many of these struggles and common questions are homogenous to the 
-        human species and have been for thousands of years. {" "}
-        <br/>
-        <span className="font-bold text-orange-700">
-        Simply put, they are what it means to be human.{" "}
-        </span>
-        Societal distractions prevent us from asking and answering these internal questions 
-        we may have.{" "}
-        <span className="font-bold text-orange-700">Our goal is to dim the noise around us {" "}</span> 
-        and aim towards truth in order to live more 
-        fulfilling lives with purpose and love. 
+      <p className="mb-3 mt-6 px-2 sm:px-0 text-center">
+      Many things in today’s world contribute to making us feel isolated in our struggles and problems, 
+      but that doesn&apos;t mean it&apos;s true. Many of these struggles and common questions are homogenous to the 
+      human species and have been for thousands of years. Simply put, they are what it means to be human. 
+      Societal influences distract us from asking and answering these internal questions we have.
+      </p>
+
+      <p className="mb-3 mt-6 px-2 sm:px-0 text-center">
+      Our goal is to dim the noise around us and aim towards truth in order to live more fulfilling lives with purpose and love.
       </p>
     </motion.section>
   );
