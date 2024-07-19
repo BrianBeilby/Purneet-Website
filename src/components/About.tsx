b67ab82975df113bbbead6139800da3338605224
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-07-19 10:46:18 -0700

    Minor Changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,15 +9,15 @@ function About() {
     <motion.section
       ref={ref}
       id="about"
-      className="mt-10 mx-4 sm:mt-28 sm:mx-auto max-w-[45rem] text-center bg-black leading-8 text-white text-lg sm:text-xl sm:mb-40 scroll-mt-5 min-h-screen flex flex-col justify-center items-center"
+      className="mt-10 mx-4 sm:mt-28 sm:mx-auto max-w-[42rem] text-center bg-black leading-8 text-white text-md sm:text-lg sm:mb-40 scroll-mt-5 min-h-screen flex flex-col justify-center items-center"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
     >
-      <h2 className="text-2xl sm:text-3xl font-medium capitalize mb-4 sm:mb-8 mt-3 text-orange-500">
+      <h2 className="text-3xl sm:text-3xl font-medium capitalize mb-10 sm:mb-8 mt-3 text-orange-500">
         Mission Statement
       </h2>
-      <p className="mb-6 px-2 sm:px-0">
+      <p className="mb-6 px-2 sm:px-0 ">
       Welcome to the Dim Eye Show. This platform is an open space for conversation and engagement about 
       topics that are often overlooked as well as a platform to explore interesting topics and subjects. 
       Common themes in our content are: society and societal impacts, technology & innovation, spirituality/religion/philosophy, 
@@ -35,7 +35,7 @@ function About() {
       </p>
 
       <p className="mb-3 mt-6 px-2 sm:px-0 text-center">
-      Our goal is to dim the noise around us and aim towards truth in order to live more fulfilling lives with purpose and love.
+      Many things in today’s world contribute to making us feel isolated in our struggles and problems, but it doesn&apos;t have to be that way.
       </p>
     </motion.section>
   );
